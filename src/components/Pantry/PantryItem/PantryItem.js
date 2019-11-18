5214ef4a255faa9240babcb81ec8403158b18726
Eric Wang <ericwang27759@gmail.com>
2019-11-17 22:38:06 -0500

    PantryItemMain not rendering

--- src/components/Pantry/PantryItem/PantryItem.js
+++ src/components/Pantry/PantryItem/PantryItem.js
@@ -2,28 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 import './PantryItem.css';
-import PantryItemMain from '../PantryItemMain/PantryItemMain';
+
 
 class PantryItem extends Component {
     render() {
         const { item } = this.props;
-        return (
-            <div className="PantryItem">
-                <h4>
-                    <Link to={`/pantry-item/${item.id}`}>
-                        {item.name}
-                    </Link>
-                </h4>
-                <div className="controller">
-                    <div className="item-modified-date">
-                        Modified {format(new Date(item.modified), 'do MMM yyyy')}
+        console.log(item.length)
+        if (item.length) {
+            return (
+                <div className="PantryItem">
+                    <h4>
+                        <Link to={`/pantry-item/${item.id}`}>
+                            {item.name}
+                        </Link>
+                    </h4>
+                    <div className="controller">
+                        <div className="item-modified-date">
+                            Modified {format(new Date(item.modified), 'do MMM yyyy')}
+                        </div>
+                        <button type="button">
+                            Delete
+                        </button>
                     </div>
-                    <button type="button">
-                        Delete
-                    </button>
                 </div>
-            </div>
-        )
+            );
+        } else {
+            return <></>
+        }
+        
     }
 }
 

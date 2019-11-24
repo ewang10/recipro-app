a209a2ec1a6c8a80757fdab85a25ab342dd964e0
Eric Wang <ericwang27759@gmail.com>
2019-11-24 14:37:52 -0500

    minor bug fix in recipe search

--- src/components/RecipeSearch/RecipeSearch.js
+++ src/components/RecipeSearch/RecipeSearch.js
@@ -38,14 +38,14 @@ class RecipeSearch extends Component {
 
         SearchRecipeApiService.getRecipes(search.value)
             .then(data => {
-                this.setState({searchResults: data.hits});
+                this.setState({ searchResults: data.hits });
 
                 if (this.state.searchResults.length !== 0) {
                     //console.log('show list true ', this.state.searchResults)
-                    this.setState({showList: true})
+                    this.setState({ showList: true })
                 } else {
                     //console.log('no results')
-                    this.setState({noResults: true})
+                    this.setState({ noResults: true })
                 }
             })
             .catch(error => console.log(error));
@@ -63,22 +63,24 @@ class RecipeSearch extends Component {
         const className = this.state.showList ? "" : "hidden";
         return (
             <section className="backgroundImg wrapper">
-                    <form
-                        className="recipe_search"
-                        onSubmit={e => this.handleSearch(e)}
-                    >
-                        <label htmlFor="search">Find something to cook</label>
-                        <input name="search" id="search"
-                            placeholder="e.g. chicken, thyme, milk..." required />
-                        <button type="submit">Search</button>
-                    </form>
-                    {this.state.noResults && 
-                            <p className="no-results">No results available</p>}
-                    <section id="results" className={className}>
-                        <ul id="results-list">
-                            {this.state.searchResults.length && this.resultList()}
-                        </ul>
-                    </section>
+                <form
+                    className="recipe_search"
+                    onSubmit={e => this.handleSearch(e)}
+                >
+                    <label htmlFor="search">Find something to cook</label>
+                    <input name="search" id="search"
+                        placeholder="e.g. chicken, thyme, milk..." required />
+                    <button type="submit">Search</button>
+                </form>
+                {this.state.noResults && (this.state.searchResults.length === 0) &&
+                    <p className="no-results">No results available</p>}
+                <section id="results" className={className}>
+                    <ul id="results-list">
+                        {(this.state.noResults === false) &&
+                            this.state.searchResults.length &&
+                            this.resultList()}
+                    </ul>
+                </section>
             </section>
         );
     }
